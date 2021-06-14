11c87f78f06d1a0fdcdf00b3408d686f99cd38f8
Owen McGill <owen.mcgill88@gmail.com>
2021-06-14 18:55:47 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+
+}
+
+function calculateCorrectAnswer(){
+
+
+}
+
+function incrementScore(){
+
+
+}
+
+function incrementWrongAnswer(){
+
+
+}
+
+function displayAdditionQuestion(){
+
+
+}
+
+function displaySubtractQuestion(){
+
+    
+}
+
+function displayMultiplyQuestion(){
+
+    
+}
+
+function displayDivisionQuestion(){
+
+    
+}
